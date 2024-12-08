71d489485b3f8bea82a39b27265e5672a5439ae0
George Stresna <68332128+georgestresna@users.noreply.github.com>
2024-12-09 00:03:12 +0200

    added selenium

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from scraper import get_game_data
+from scraper import principal
 
-# Exemplu de DataFrame cu datele jocurilor
-"""
-data = pd.DataFrame({
-    'price': [19.99, 29.99, 49.99, 9.99],
-    'rating': [4.5, 4.7, 3.8, 4.0],
-    'num_reviews': [1000, 2500, 500, 1500],
-})
 
-# Scalarea prețului, ratingului și numărului de recenzii
-scaler = MinMaxScaler()
-data[['price', 'rating', 'num_reviews']] = scaler.fit_transform(data[['price', 'rating', 'num_reviews']])
-
-print(data)
-"""
-
-get_game_data("https://steamdb.info/")
+principal()
--- scraper.py
+++ scraper.py
@@ -1,13 +1,11 @@
-#scraper
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-import requests
-from bs4 import BeautifulSoup
-
-def get_game_data(url):
-    response=requests.get(url)
-    soup=BeautifulSoup(response.text,'html.parser')
-
-    pretty=soup.prettify()
-
-    print(pretty)
-
+def principal():
+    options = webdriver.ChromeOptions()
+    #options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(5)
+    driver.get("https://steamdb.info/stats/globaltopsellers/")
+    time.sleep(10)
