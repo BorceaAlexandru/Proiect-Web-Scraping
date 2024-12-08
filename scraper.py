859bb9e501058d6b09bd113f43b9bfe04dc8e07b
Borcea Alex <alxborcea@gmail.com>
2024-12-08 23:51:20 +0200

    nimic

--- scraper.py
+++ scraper.py
@@ -6,5 +6,6 @@ from bs4 import BeautifulSoup
 def get_game_data(url):
     response=requests.get(url)
     soup=BeautifulSoup(response.text,'html.parser')
+    pretty=soup.prettify()
 
-    print(soup)
+    print(pretty)
