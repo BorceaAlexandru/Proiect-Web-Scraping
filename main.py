0f603e38e04c562fa1c3b0bda35ddec3f6315332
Borcea Alex <alxborcea@gmail.com>
2024-12-08 23:36:10 +0200

    add scraper

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from bs4 import BeautifulSoup
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
-print("Hello")
+# Exemplu de DataFrame cu datele jocurilor
+data = pd.DataFrame({
+    'price': [19.99, 29.99, 49.99, 9.99],
+    'rating': [4.5, 4.7, 3.8, 4.0],
+    'num_reviews': [1000, 2500, 500, 1500],
+})
 
-def game():
-    print("ceva")
+# Scalarea prețului, ratingului și numărului de recenzii
+scaler = MinMaxScaler()
+data[['price', 'rating', 'num_reviews']] = scaler.fit_transform(data[['price', 'rating', 'num_reviews']])
 
-game()
+print(data)
